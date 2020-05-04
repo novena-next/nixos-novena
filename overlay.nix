d2348cd73441e4d6221475fec4d0bad885bdd036
Richard Marko <srk@48.io>
2020-05-04 08:24:09 +0200

    kernel: switch to structured config

--- overlay.nix
+++ overlay.nix
@@ -8,7 +8,27 @@ in
     novena-usb-hub = callPackage ./novena-usb-hub {};
 
     linux_nvn = callPackage ./kernel {
-      kernelPatches = [];
+      # with final.lib.kernel?
+      structuredExtraConfig = with import <nixpkgs/lib/kernel.nix> { inherit (super) lib; version = super.linux_nvn.version; }; {
+        DRM_ETNAVIV = module;
+        DRM_ETNAVIV_THERMAL = yes;
+        DRM_ITE_IT6251 = module;
+        USB_NET_AX8817X = module;
+        SND_SOC_IMX_ES8328 = module;
+
+        REGULATOR_PFUZE100 = module;
+        # NET_ETHERNET m
+        USB_SERIAL_PL2303 = module;
+        USB_SERIAL_CH341 = module;
+
+        # ZZZ
+        DRM_NOUVEAU = no;
+        DRM_RADEON = no;
+        DRM_EXYNOS = no;
+        E1000E = no;
+        IGB = no;
+      };
+      kernelPatches = [ ];
     };
     linuxPackages_nvn = linuxPackagesFor final.linux_nvn;
 
