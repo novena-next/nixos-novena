a189b83d439720b6e209ca257ea43bdb3335ccde
Richard Marko <srk@48.io>
2020-11-12 14:14:32 +0100

    clean-up cruft/desktop.nix

--- cruft/desktop.nix
+++ cruft/desktop.nix
@@ -1,12 +1,15 @@
 { config, lib, pkgs, ... }:
 
+# lightdm + i3 example
+
 {
 
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
 
+  # if you want to try sway
   programs.sway = {
-   enable = true;
+   enable = false;
    extraPackages = with pkgs; [ swaylock swayidle xwayland rxvt_unicode dmenu ];
   };
 
@@ -26,27 +29,19 @@
       Option "kmsdev" "/dev/dri/card1"
       Option "AccelMethod" "none"
     '';
-    # in etc
+
+    # so it's in etc
     exportConfiguration = true;
 
-    #displayManager.startx.enable = true;
     displayManager.lightdm.enable = true;
-    #displayManager.defaultSession = "sway";
 
-    #desktopManager.default      = "none";   # the plain xmonad experience
     desktopManager.xterm.enable = true;
     windowManager.i3 = {
       enable = true;
     };
-
-    #windowManager.default       = "none"; # sets it as default
-    #windowManager.default       = "xmonad"; # sets it as default
-    #windowManager.xmonad = {
-    #  enable = true;     # installs xmonad and makes it available
-    #  enableContribAndExtras = true; # makes xmonad-contrib and xmonad-extras available
-    #};
   };
-    # use fc-list to list fonts
+
+  # use fc-list to list fonts
   fonts.fonts = with pkgs; [
       anonymousPro
       #corefonts
